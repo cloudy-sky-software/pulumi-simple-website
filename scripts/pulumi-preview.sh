4959cf3e70a6ee371a782b84c183d362c75e2c9b
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2020-01-16 10:14:19 -0800

    Add scripts folder. Add gitlab-ci config. Add Pulumi stack config file.

---
+++ scripts/pulumi-preview.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# exit if a command returns a non-zero exit code and also print the commands and their args as they are executed
+set -e -x
+
+# Add the pulumi CLI to the PATH
+export PATH=$PATH:$HOME/.pulumi/bin
+
+yarn install
+
+pulumi stack select praneetloke/s3
+pulumi config set aws:region us-west-2
+pulumi preview
